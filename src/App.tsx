d410a66942a58e95cbe2fd1ce3dd4623d4f5e00f
markbaguio <godwinbaguio@gmail.com>
2025-01-19 15:21:46 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { PageHeader } from "./assets/components/PageHeader";
+
+function App() {
+  return (
+    <>
+      <PageHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ src/assets/components/PageHeader.tsx
@@ -0,0 +1,7 @@
+export function PageHeader() {
+  return (
+    <>
+      <h1>Page Header</h1>
+    </>
+  );
+}
