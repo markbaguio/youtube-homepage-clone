89de9df8570a57ff4806f819d488f4f4084a3c69
markbaguio <godwinbaguio@gmail.com>
2025-01-20 12:43:24 +0800

    Remove jsx

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,17 +1,3 @@
-import { Menu } from "lucide-react";
-
 export function PageHeader() {
-  return (
-    <>
-      <div className="flex gap-10 justify-between">
-        <div className="flex gap-4 lg:gap-20 justify-normal">
-          <button>
-            <Menu />
-          </button>
-          <a href="/">asdasd</a>
-        </div>
-        <div className="2"></div>
-      </div>
-    </>
-  );
+  return <></>;
 }
