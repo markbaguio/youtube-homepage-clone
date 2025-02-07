2f6e1f19c59845cff286bcb55d675b6c1cb4ebbc
Mark Godwin C. Baguio <godwinbaguio@gmail.com>
2025-02-07 15:20:04 +0800

    WIP:Add Time ago formatter and views formatter

--- src/components/VideoGridItem.tsx
+++ src/components/VideoGridItem.tsx
@@ -1,5 +1,7 @@
 import { Channel } from "../dummy-data/home";
 import { formatDuration } from "../utils/formatDuration";
+import { formatTimeAgo } from "../utils/formatTimeAgo";
+import { VIEW_FORMATTER } from "../utils/formatViews";
 
 type VideoGridItemProps = {
   id: string;
@@ -40,6 +42,28 @@ export default function VideoGridItem({
           {formatDuration(duration)}
         </div>
       </a>
+      <div className="flex gap-2">
+        <a href={`/@${channel.id}`} className="flex-shrink-0">
+          <img
+            src={channel.profileUrl}
+            alt="Profile ID"
+            className="w-12 h-12 rounded-full"
+          />
+        </a>
+        <div className="flex flex-col">
+          <a href={`/watch?v=${id}`} className="font-bold">
+            {title}
+          </a>
+          <a href={`/@${channel.id}`} className="text-secondary-texte text-sm">
+            {channel.name}
+          </a>
+          <div className="text-secondary-text text-sm">
+            {`${VIEW_FORMATTER.format(views)} views • ${formatTimeAgo(
+              postedAt
+            )}`}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
